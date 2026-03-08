383736bac1a3756c371c9ab39fa0558b34cd7768
aj-bei <aureliobeiza2@gmail.com>
2026-03-08 15:39:08 -0400

    finalized code mapping JSON and began work on Judges dataset

--- scripts/data_download.py
+++ scripts/data_download.py
@@ -17,7 +17,7 @@ def download_files_from_fld(folder_id: str, destination: str) -> None:
     url = f"https://drive.google.com/drive/folders/{folder_id}"
     fs_in_drive = set([f.path for f in gdown.download_folder(url, use_cookies=False, skip_download=True, quiet=True)])
     
-    if set(os.listdir(destination)) > fs_in_drive:  # destination has all files plus extras
+    if set(os.listdir(destination)) > fs_in_drive:  # destination is superset of files in drive
         print("All files already downloaded.")
         return
     print(f"Downloading files: {fs_in_drive - set(os.listdir(destination))}")
@@ -25,7 +25,7 @@ def download_files_from_fld(folder_id: str, destination: str) -> None:
 
 
 if __name__ == "__main__":
-    # only runs if running this file directly
+    # for testing
     folder_id = "1P2FRAkPrqL2nn2MNMyd4ilWbXNS_kkKD"
     destination = os.path.join(os.path.dirname(__file__), "../data")
     download_files_from_fld(folder_id, destination)
